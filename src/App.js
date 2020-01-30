31d9ff32a51e912f88cf5afe93bb5814606143ff
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-30 15:53:49 -0800

    Basics completed, formik form is functional.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormikOnboardForm from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikOnboardForm />
+    </div>
+  );
+}
+
+export default App;
