7d2079d67febcaac20da4aedc4b8dbf46a7fc718
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-30 22:18:43 -0800

    Still working, one more validation to add and some styling completed using components and css

--- src/Form.js
+++ src/Form.js
@@ -2,68 +2,102 @@ import { withFormik, Form, Field } from "formik";
 import React, { useState, useEffect } from 'react';
 import * as Yup from 'yup';
 import axios from 'axios';
+import {Row, Col} from 'reactstrap';
+import {FlexForm, Header, Competitors} from './components';
+import './form.css';
+
 const OnboardForm = ({ values, errors, touched, status }) => {
     const [ person, setPerson ] = useState([]);
     useEffect(() => {
         status && setPerson(person => [...person, status]);
     }, [status]);
     return (
-        <div className='onboard-form'>
-            <Form>
-                <label htmlFor="people">
-                    Name:
-                    <Field 
-                    id="people" 
-                    type="text" 
-                    name="people" 
-                    placeHolder="Enter Name"
-                    />
-                    {touched.people && errors.people && (
-                        <p className="errors">{errors.people}</p>
-                    )}
-                </label>
-                <label>
-                    Email:
-                    <Field
-                    id="email"
-                    type="text"
-                    name="email"
-                    placeHolder="Enter Email"
-                    />
-                    {touched.email && errors.email && (
-                        <p className="errors">{errors.email}</p>
-                    )}
-                </label>
-                <label htmlFor="password">
-                    Password:
-                    <Field
-                    id="password"
-                    type="password"
-                    name="password"
-                    placeHolder="Enter Password"
-                    />
-                    {touched.password && errors.password && (
-                        <p className="errors">{errors.password}</p>
-                    )}
-                </label>
-                <label>
-                    Terms of Service:
-                    <Field
-                    type="checkbox"
-                    name="tos"
-                    checked={values.tos}
-                    />
-                </label>
-                <button type="submit">Submit</button>
-            </Form>
-            {person.map(persons => {
-                return (
-                   <ul key={persons.id}>
-                       <li>Name: {persons.people}</li>
-                       <li>Email: {persons.email}</li>
-                   </ul>     
-                )
-            })}
+        <div>
+            {/* <img src="dc-logo.png"/> */}
+            <Header>Capture да Flag!</Header>
+            <FlexForm>
+                <Col>
+                    <Form className="flexContainer">
+                        <h2>Sign Up: </h2>
+                        <Row>
+                            <label htmlFor="people">
+                                Name:
+                                <Field 
+                                id="people" 
+                                type="text" 
+                                name="people" 
+                                placeHolder="Enter Name"
+                                />
+                                {touched.people && errors.people && (
+                                    <p className="errors">{errors.people}</p>
+                                )}
+                            </label>
+                        </Row>
+
+                        <Row>
+                        <label>
+                            Email:
+                            <Field
+                            
+                            id="email"
+                            type="text"
+                            name="email"
+                            placeHolder="Enter Email"
+                            />
+                            {touched.email && errors.email && (
+                                <p className="errors">{errors.email}</p>
+                            )}
+                        </label>
+                        </Row>
+
+                        <Row>
+                        <label htmlFor="password">
+                            Password:
+                            <Field
+                            className="field"
+                            id="password"
+                            type="password"
+                            name="password"
+                            placeHolder="Enter Password"
+                            />
+                            {touched.password && errors.password && (
+                                <p className="errors">{errors.password}</p>
+                            )}
+                        </label>
+                        </Row>
+
+                        <Row>
+                        <label>
+                            Terms of Service:
+                            <Field
+                            className="field"
+                            type="checkbox"
+                            name="tos"
+                            checked={values.tos}
+                            />
+                        </label>
+                        </Row>
+
+                        <Row className="buttonDiv">
+                        <button type="submit">Submit</button>
+                        </Row>
+                    </Form>
+                </Col>                
+            </FlexForm>
+                <div className="cardContainer">
+                    <Competitors>Current Competitors:</Competitors>
+                    <div className="userCard">
+                        {person.map(persons => {
+                            return (
+                            <div className="userFrame" key={persons.id}>
+                                <p>Name: {persons.people}</p>
+                                <p>Email: {persons.email}</p>
+                            </div>     
+                            )
+                        })}
+                    </div>
+                </div>
+            
         </div>
     );
 };
@@ -77,9 +111,9 @@ const FormikOnboardForm = withFormik ({
         };
     },
     validationSchema: Yup.object().shape({
-        people: Yup.string().required("Name is required"),
-        email: Yup.string().required("Email is required"),
-        password: Yup.string().required("Password is required")
+        people: Yup.string().required("Name is required!"),
+        email: Yup.string().required("Email is required!"),
+        password: Yup.string().required("Password is required!")
     }),
     handleSubmit( values, { setStatus, resetForm }) {
         axios.post("https://reqres.in/api/users", values)
